42a3bf17b5eee8b16aa974bac13b5f2f0cc46817
Yves Kunz <yves.kunz@gmail.com>
2015-01-03 19:45:01 +0100

    updated vimrc for vim-slime

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,4 +1,12 @@
-rm -f ~/installed_packages.txt ~/backup.tgz
-dpkg --get-selections > ~/installed_packages.txt
-tar czf /home/yek/backup.tgz installed_packages.txt code bin projects Dokumente schemlib.fig .emacs --exclude-backups
-echo "yek laptop backup"| mutt yek@kunzy.net -s "yek backup" -a "/home/yek/backup.tgz"
+cd
+rm -f installed_packages.txt
+rm -f backup.tar
+dpkg --get-selections > installed_packages.txt
+tar cf backup.tar installed_packages.txt
+tar rf backup.tar code --exclude='code/android-sdk/*'
+tar rf backup.tar projekte
+tar rf backup.tar Dokumente
+tar rf backup.tar Bitwig\ Studio
+tar rf backup.tar dotfiles
+gzip -f backup.tar
+#echo "yek laptop backup"| mutt yek@kunzy.net -s "yek backup" -a "/home/yek/backup.tgz"
--- bin/backuppackagelist.sh
+++
@@ -1,3 +0,0 @@
-rm -f ~/installed_packages.txt
-dpkg --get-selections > ~/installed_packages.txt
-echo "linux package list backup packages apt dpkg"| mutt yek@kunzy.net -s "package list backup" -a "installed_packages.txt"
